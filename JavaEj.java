71550516d8ea0ff1fad25124e90ec6b18a6cba67
Matmart13 <mmmancini13@gmail.com>
2023-04-17 17:26:46 +0200

    Cambio a Eclipse

--- JavaEj.java
+++ JavaEj.java
@@ -1,5 +1,6 @@
 public class JavaEj {
    public static void main(String[] args) {
     System.out.println("Hola caracola para ti mi cola, mancini la chupa");
+    System.out.println("Alberto la Chupa");
     }
 }
