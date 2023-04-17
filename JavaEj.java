4036bcb1ee3e163c47e43f8156f77c6106c46b11
AlbertoGS2001 <alberto.gutierrez.sanchez@colegiolitterator.com>
2023-04-17 17:24:50 +0200

    actualizacion

--- JavaEj.java
+++ JavaEj.java
@@ -1,5 +1,5 @@
 public class JavaEj {
    public static void main(String[] args) {
-    System.out.println("Hola caracola para ti mi cola");
+    System.out.println("Hola caracola para ti mi cola, mancini la chupa");
     }
 }
